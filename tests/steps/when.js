b18a08c14319d7ec512c71cd4d023ef9b3cc91c1
Werz, Patrick <patrick.werz@zoi.de>
2021-01-08 14:00:58 +0100

    add eventbridge

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -12,7 +12,7 @@ const viaHandler = async (event, functionName) => {
     const context = {}
     const response = await handler(event, context)
     const contentType = _.get(response, 'headers.content-type', 'application/json');
-    if (response.body && contentType === 'application/json') {
+    if (_.get(response, 'body') && contentType === 'application/json') {
         response.body = JSON.parse(response.body);
     }
     return response
@@ -103,9 +103,32 @@ const viaHttp = async (relPath, method, opts) => {
         }
     }
 }
+const we_invoke_place_order = async (user, restaurantName) => {
+    const body = JSON.stringify({ restaurantName })
+
+    switch (mode) {
+        case 'handler':
+            return await viaHandler({ body }, 'place-order')
+        case 'http':
+            const auth = user.idToken
+            return await viaHttp('orders', 'POST', { body, auth })
+        default:
+            throw new Error(`unsupported mode: ${mode}`)
+    }
+}
+
+const we_invoke_notify_restaurant = async (event) => {
+    if (mode === 'handler') {
+        await viaHandler(event, 'notify-restaurant')
+    } else {
+        throw new Error('not supported')
+    }
+}
 
 module.exports = {
     we_invoke_get_index,
     we_invoke_get_restaurants,
-    we_invoke_search_restaurants
+    we_invoke_search_restaurants,
+    we_invoke_place_order,
+    we_invoke_notify_restaurant
 }
